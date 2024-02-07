ced66d8fb3df53e1fe403ae5a46a64bcdd9f3ff3
Fernando Martinez <a2308749@institutmontilivi.cat>
2024-02-07 20:07:54 +0100

    segundo cambio

--- AC/Program.cs
+++ AC/Program.cs
@@ -8,7 +8,7 @@
             ///1. ANIMALES
             ///2. CAPITALES
             ///3. CANCIONES
-            ///TOPICS FERNANDO
+            ///TOPICS FER
             ///1. FURBOLISTAS
             ///2. ESTADIOS
             ///3. ENTRENADORES
@@ -16,7 +16,43 @@
             ///1. INSTRUMENTOS MUSICALES
             ///2. DEPORTES
             ///3. HISTORIA
-           
+            ConsoleKeyInfo tecla;
+            do
+            {
+                Console.Clear();
+                ShowOptions();
+                tecla = Console.ReadKey();
+                Console.Clear();
+                switch (tecla.Key)
+                {
+                    case ConsoleKey.D1:
+                        YARYNA();
+                        break;
+                    case ConsoleKey.D2:
+                        FERNANDO();
+                        break;
+                    case ConsoleKey.D3:
+                        PRESLAVA();
+                        break;
+                    case ConsoleKey.D0:
+                        MsgNextScreen("Error. Prem una tecla per tornar al menú...");
+                        break;
+                }
+            } while (tecla.Key != ConsoleKey.D0);
         }
+
+        public static void ShowOptions()
+        {
+            Console.Clear();
+            Console.WriteLine("1 - CUSTOMERS");
+            Console.WriteLine("2 - PROVIDERS");
+            Console.WriteLine("3 - INVOICES");
+            Console.WriteLine("5 - DATA");
+            Console.WriteLine("6 - MCD");
+            Console.WriteLine("7 - Hulukulu y Bulukulu");
+            Console.WriteLine("8 - Suma de dígitos");
+            Console.WriteLine("0 - EXIT");
+        }
+
     }
 }
