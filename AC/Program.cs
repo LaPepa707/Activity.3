1d3d3bf57422b4f293df58fa02a7ad4b957feac0
presi <aleksandrova24p.r@gmail.com>
2024-02-07 19:24:52 +0100

    Agregar archivos de proyecto.

---
+++ AC/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace AC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ///TOPICS YARYNA
+            ///1. ANIMALES
+            ///2. CAPITALES
+            ///3. CANCIONES
+            ///TOPICS FER
+            ///1. FURBOLISTAS
+            ///2. ESTADIOS
+            ///3. ENTRENADORES
+            ///TOPCIS PRES
+            ///1. INSTRUMENTOS MUSICALES
+            ///2. DEPORTES
+            ///3. HISTORIA
+           
+        }
+    }
+}
